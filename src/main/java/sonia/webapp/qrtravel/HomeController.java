dfee26c74d37304715bab107a08fe008aaae58d3
Thorsten Ludewig <t.ludewig@ostfalia.de>
2020-06-22 13:55:49 +0200

    read values from cookie and show it on website

--- src/main/java/sonia/webapp/qrtravel/HomeController.java
+++ src/main/java/sonia/webapp/qrtravel/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import static sonia.webapp.qrtravel.QrTravelToken.QR_TRAVEL_TOKEN;
 import static sonia.webapp.qrtravel.QrTravelToken.UNKNOWN_TOKEN;
 
@@ -25,21 +26,35 @@ public class HomeController
   @GetMapping("/")
   public String home(@RequestHeader(name = "x-original-uri", required = false,
                                       defaultValue = "") String originalUri,
-    @CookieValue(value = QR_TRAVEL_TOKEN,
-                 defaultValue = UNKNOWN_TOKEN) String tokenValue,
+    @RequestParam( name="p", required = false ) String pin,
+    @CookieValue(value = QR_TRAVEL_TOKEN, defaultValue = UNKNOWN_TOKEN) String tokenValue,
     HttpServletResponse response, Model model)
   {
-    System.out.println(" - get login - x-original-uri=" + originalUri);
-    
     QrTravelToken token = QrTravelToken.fromCookieValue(tokenValue);
     
+    LOGGER.info( "pin = " + pin );
+    
+    LOGGER.info( "token mail = " + token.getMail() );
+    LOGGER.info( "token phone = " + token.getPhone() );
+    LOGGER.info( "token surname = " + token.getSureName() );
+    LOGGER.info( "token given name = " + token.getGivenName() );
+    
     /*
-    AuthToken token = AuthToken.fromCookieValue(tokenValue, originalUri);
-    AuthController.addTokenToHttpServletResponse(token, response);
-    model.addAttribute("counter", token.getLoginCounter() + 1);
-    model.addAttribute("authServiceUri", CONFIG.getWebServiceUrl());
+    token.setMail("t.ludewig@ostfalia.de");
+    token.setPhone("+49 5331 939 19000");
+    token.setGivenName("Thorsten");
+    token.setSureName("Ludewig");
     */
     
+    model.addAttribute("pin", pin );
+    model.addAttribute("mail", token.getMail());
+    model.addAttribute("phone", token.getPhone());
+    model.addAttribute("surname", token.getSureName());
+    model.addAttribute("givenname", token.getGivenName());
+   
+   //  model.addAttribute("description", description );
+   //  model.addAttribute("studentNumber", studentNumber );
+ 
     token.addToHttpServletResponse(response);
     return "home";
   }
